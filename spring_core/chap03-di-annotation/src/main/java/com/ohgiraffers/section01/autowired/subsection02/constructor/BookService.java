3a0371c2e07268f803a9f5270501421843887fe0
SaeRyung <sr1094@naver.com>
2024-08-30 00:58:09 +0900

    [spring]chap03-di-annotation-constructor 학습

--- spring_core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/autowired/subsection02/constructor/BookService.java
+++ spring_core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/autowired/subsection02/constructor/BookService.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.section01.autowired.subsection02.constructor;
 
 import com.ohgiraffers.section01.autowired.common.BookDAO;
 import com.ohgiraffers.section01.autowired.common.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Service("bookServiceConstructor")
 public class BookService {
 
+    // BookDAO 클래스를 의존
     public BookDAO bookDAO;
 
     // 기본생성자 2개이면 뭘로 생성할지 정해지지 않았기에 에러,
@@ -18,16 +20,15 @@ public class BookService {
     /* 생성자 주입 - 생성자 1개만 가능하다.*/
     /*Spring 4.3 버전 이후부터 생성자가 1개 뿐이라면 어노테이션을 생략해도 자동으로 생성자 주입이 동작한다.
     * 단, 생성자가 1개 이상인 경우에는 명시적으로 작성해주서야 한다. */
-//    @Autowired
+    @Autowired
     public BookService(BookDAO bookDAO) {
         this.bookDAO = bookDAO;
     }
 
-
-
     public List<BookDTO> selectAllBooks(){
         return bookDAO.selectBookList();
     }
+
     public BookDTO searchBookBySequence(int sequence){
         return bookDAO.selectOneBook(sequence);
     }
