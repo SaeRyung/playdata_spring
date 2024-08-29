d7a2f9276aaa324db1d83dc79382ed21ad4e9e3d
SaeRyung <sr1094@naver.com>
2024-08-29 22:18:05 +0900

    [spring]chap01-ioc AnnotationConfigApplicationContext 학습

--- spring_core/chap01-ioc-container/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
+++ spring_core/chap01-ioc-container/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -4,12 +4,12 @@ import com.ohgiraffers.common.MemberDTO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.lang.reflect.Member;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationContext.class);
-        //클래스에 ConfigurationContext 메타정보 전달
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(ConfigurationContext.class);
+        //클래스에 ConfigurationContext.class 메타정보 전달
 
         MemberDTO member = context.getBean("member", MemberDTO.class);
         System.out.println(member);
