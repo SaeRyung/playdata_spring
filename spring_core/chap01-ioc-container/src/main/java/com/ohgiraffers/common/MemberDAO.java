cd62ebba4d237ad04a710c9e50ea1e631c29a332
SaeRyung <sr1094@naver.com>
2024-08-31 01:18:56 +0900

    [spring]chap01-ioc 학습

--- spring_core/chap01-ioc-container/src/main/java/com/ohgiraffers/common/MemberDAO.java
+++ spring_core/chap01-ioc-container/src/main/java/com/ohgiraffers/common/MemberDAO.java
@@ -8,6 +8,7 @@ import java.util.Map;
 /* 컴포넌트 스캔을 통해 빈으로 등록 될 수 있도록 함 */
 //@Component -> ioc container가 스캔하여 빈으로 등록된다.
 @Component
+// 개발자가 직접 제어할 수 없는 외부 라이브러리는 @Bean 메소드를 사용해 수동 빈 추가 ,,, 다형성 활용하고 싶을 때도 @Bean 사용
 public class MemberDAO {
 
     private final Map<Integer, MemberDTO> memberMap;
--- spring_core/chap01-ioc-container/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection02/xml/Application.java
+++ spring_core/chap01-ioc-container/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection02/xml/Application.java
@@ -5,7 +5,9 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new GenericXmlApplicationContext("section03/annotationconfig/spring-context.xml");
+        ApplicationContext applicationContext
+                //GenericXmlApplicationContext: 전달하는 메타데이터가 xml 형식이다.
+                = new GenericXmlApplicationContext("section03/annotationconfig/spring-context.xml");
 
         String[] beanNames = applicationContext.getBeanDefinitionNames();
         for(String beanName : beanNames) {
