a496f390fe1284cd1d70c9a0ec370554feb1717c
SaeRyung <sr1094@naver.com>
2024-08-31 07:40:51 +0900

    [spring]chap02-dependency-injection 학습

--- spring_core/chap02-dependency-injection/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
+++ spring_core/chap02-dependency-injection/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -3,7 +3,6 @@ package com.ohgiraffers.section02.javaconfig;
 import com.ohgiraffers.common.MemberDTO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
