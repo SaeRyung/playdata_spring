8041ceb489b920441ee2e08c7072831168f94f65
SaeRyung <sr1094@naver.com>
2024-08-31 09:31:00 +0900

    [spring]chap03-di-annotation 인터페이스 상속 학습

--- spring_core/chap03-di-annotation/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/PokemonService.java
+++ spring_core/chap03-di-annotation/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/PokemonService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service("pokemonServicePrimary")
 public class PokemonService {
 
-    // Pokemon 생성자 선언 후
+    // 생성자 필드 선언
     private Pokemon pokemon;
 
     // @Autowired: 타입을 기준으로 가져온다. -> Charmander에 @Primary 작성함
