488b9ee3f6d17638ae03ab443f523ee33f94cd2c
SaeRyung <sr1094@naver.com>
2024-09-18 20:04:35 +0900

    [JDBC]chap03 crud 학습

--- JDBC/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section02/update/MenuRepository.java
+++ JDBC/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section02/update/MenuRepository.java
@@ -12,6 +12,7 @@ import static com.ohgiraffers.common.JDBCTemplate.close;
 public class MenuRepository {
     int updateMenu(Connection con, Menu menu){
         PreparedStatement ps = null;
+        // 1. PreparedStatement 를 통해
 
         int result = 0;
 
@@ -20,12 +21,13 @@ public class MenuRepository {
             props.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/section01/insert/mapper/menu-mapper.xml"));
             String sql = props.getProperty("updateMenu");
 
-            ps = con.prepareStatement(sql);
+            ps = con.prepareStatement(sql); // 2. 수행하고 싶은 sql 셋팅
+            // 3. 셋팅된 구문에서 물음표 위치에 따라 전달받은 객체
             ps.setInt(1, menu.getMenuCode());
             ps.setString(2, menu.getMenuName());
             ps.setInt(3, menu.getMenuPrice());
 
-            result = ps.executeUpdate();
+            result = ps.executeUpdate(); // 4. 실행
 
             } catch (IOException e) {
             throw new RuntimeException(e);
